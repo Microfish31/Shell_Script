b33ef16b1cf7ddb1bf64f2507312aa4ee9461f40
Microfish31 <czsea35@yahoo.com.tw>
2022-07-05 15:08:15 +0800

    Update git_create.sh
    YOUR_GITHUB_TOKEN

--- git_repo/git_create.sh
+++ git_repo/git_create.sh
@@ -1,5 +1,5 @@
 curl -X POST \
 -H "Accept: application/vnd.github+json" \
--H "Authorization: token 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" \
+-H "Authorization: token YOUR_GITHUB_TOKEN" \
 -d '{"name":"Shell_Script","description":"shell script","private":true}' \
 'https://api.github.com/user/repos'
